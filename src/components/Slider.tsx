3322272386eac3fae126d6b71e411b22332f5ec4
tbs01215 <tbs01215@egmail.com>
2023-06-23 14:01:10 +0900

    feat: Create MovieInfo to display movie title on hover in slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -52,6 +52,18 @@ const NavButton = styled.button`
     background: rgba(0, 0, 0, 0.9);
   }
 `;
+const MovieInfo = styled(motion.div)`
+  padding: 20px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h3 {
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+`;
 
 const PrevButton = styled(NavButton)`
   left: 10px;
@@ -65,6 +77,16 @@ const movieVariants = {
   hover: {
     scale: 1.25,
     y: -20,
+    transition: {
+      delay: 0.375,
+      type: "tween",
+      duration: 0.25,
+    },
+  },
+};
+const infoVariants = {
+  hover: {
+    opacity: 1,
     transition: {
       delay: 0.375,
       type: "tween",
@@ -133,7 +155,11 @@ export default function Slider({ movies }: SliderProps) {
                 whileHover="hover"
                 bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
                 transition={{ type: "tween" }}
-              ></Movie>
+              >
+                <MovieInfo variants={infoVariants}>
+                  <h3>{movie.title}</h3>
+                </MovieInfo>
+              </Movie>
             ))}
         </Row>
       </AnimatePresence>
